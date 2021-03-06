4c7d971ef8c256d84d5b90a421cd163c184372ff
vethiga <vethirajah96@gmail.com>
2021-03-06 23:59:27 +0530

    AddToCartVerification-Commit

--- SeleniumMaven/src/main/java/com/web/pom/POMFile_SearchFeature.java
+++ SeleniumMaven/src/main/java/com/web/pom/POMFile_SearchFeature.java
@@ -3,9 +3,7 @@ package com.web.pom;
 import java.util.List;
 
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 
 /**
 * @author Vethiga
